7ea6f3dbd68cd201a02c5e630b515141b90eadfd
Your Name01~ <you@example.com>
2023-09-21 05:25:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/textdetection/rostrosDetector.java
@@ -0,0 +1,23 @@
+package com.example.textdetection;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.face.Face;
+
+import java.util.List;
+
+public class rostrosDetector implements OnSuccessListener<List<Face>>, OnFailureListener {
+    @Override
+    public void onFailure(@NonNull Exception e) {
+        
+    }
+
+
+
+    @Override
+    public void onSuccess(List<Face> faces) {
+
+    }
+}
